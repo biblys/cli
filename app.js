0ea35bfbc2115cf354b1f5fc88113c3c5110831c
Clément Latzarus <c@iwzr.fr>
2023-03-16 19:46:03 +0100

    feat: add deploy command

---
+++ app.js
@@ -0,0 +1,21 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { execa } from 'execa';
+
+yargs(hideBin(process.argv)).version(false)
+  .command('deploy [site] [version]', 'deploy a single site', (yargs) => {
+    return yargs
+      .positional('site', {
+        describe: 'site to deploy',
+        type: 'string',
+      })
+      .positional('version', {
+        describe: 'biblys version to deploy',
+        type: 'string',
+      })
+  }, async ({ site, version }) => {
+    const { stdout } = await execa('./scripts/deploy', [site, version]);
+    console.log(stdout);
+  })
+  .parse()
+
