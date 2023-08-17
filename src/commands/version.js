fec1ca6bcf4fb181ab43109cda30721bdbe40603
Clément Latzarus <c@ltzr.io>
2023-08-17 22:02:27 +0200

    feat: allow to display multiple (but not all) sites version

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,13 +1,19 @@
 import ssh from "../services/ssh.js";
 import chalk from "chalk";
 
-async function versionCommand(site) {
-  if (site === "all") {
-    await _displayAllSitesVersion();
+async function versionCommand(target) {
+  if (target === "all") {
+    await _executeCommandForAllSites(_displaySiteVersion);
     return;
   }
 
-  await _displaySiteVersion(site);
+  if (target.includes(',')) {
+    const sites = target.split(',');
+    await _executeCommandForSomeSites(_displaySiteVersion, sites);
+    return;
+  }
+
+  await _displaySiteVersion(target);
 }
 
 async function _displaySiteVersion(site) {
@@ -15,11 +21,15 @@ async function _displaySiteVersion(site) {
   console.log(`ⓘ️ Version ${chalk.yellow(version)} is deployed on site ${chalk.blue(site)}`);
 }
 
-async function _displayAllSitesVersion() {
+async function _executeCommandForAllSites(command) {
   const sitesList = await ssh.getSitesList();
   const sites = sitesList.split(/\r?\n/);
+  await _executeCommandForSomeSites(command, sites);
+}
+
+async function _executeCommandForSomeSites(command, sites) {
   for (const site of sites) {
-    await _displaySiteVersion(site);
+    await command(site);
   }
 }
 
